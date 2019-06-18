e3548ff7e44421f4d92d9044992c509f9d183735
nakano yosuke <naninuneno0411@gmail.com>
2019-06-19 01:28:50 +0900

    top scene transition

---
+++ GrayBlue_Games/Assets/Scripts/TopScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace GrayBlue_Games {
+    public class TopScene : MonoBehaviour {
+        [SerializeField] Button testSceneButton;
+        [SerializeField] Button shootingSceneButton;
+        void Start() {
+            testSceneButton?.onClick.AddListener(() => {
+                SceneManager.LoadScene("BLETest");
+            });
+            shootingSceneButton?.onClick.AddListener(() => {
+                SceneManager.LoadScene("GyroShooting");
+            });
+        }
+    }
+}
