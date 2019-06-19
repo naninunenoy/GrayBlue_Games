36e3d1c8075de6667c683e36f06823a8e60fca7e
nakano yosuke <naninuneno0411@gmail.com>
2019-06-20 02:23:47 +0900

    add M5StackImage

--- GrayBlue_Games/Assets/Scripts/TopScene.cs
+++ GrayBlue_Games/Assets/Scripts/TopScene.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using GrayBlue;
 
 namespace GrayBlue_Games {
     public class TopScene : MonoBehaviour {
+        [SerializeField] Button grayBlueButton;
+        [SerializeField] M5StackImage m5StackImage;
         [SerializeField] Button testSceneButton;
         [SerializeField] Button shootingSceneButton;
-        void Start() {
+        Central grayBlue = default;
+
+        void Awake() {
             testSceneButton?.onClick.AddListener(() => {
                 SceneManager.LoadScene("BLETest");
             });
             shootingSceneButton?.onClick.AddListener(() => {
                 SceneManager.LoadScene("GyroShooting");
             });
+            grayBlue = Central.Instance;
         }
     }
 }
---
+++ GrayBlue_Games/Assets/Scripts/UI/M5StackImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GrayBlue;
+
+namespace GrayBlue_Games {
+    public class M5StackImage : MonoPeripheralBase {
+        [SerializeField] Text messageText;
+        [SerializeField] Image buttonAImage;
+        [SerializeField] Image buttonBImage;
+        [SerializeField] Image buttonCImage;
+
+        void Start() {
+
+        }
+
+        public void SetMessage(string message) {
+            messageText.text = message;
+        }
+    }
+}
