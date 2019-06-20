962deef25cf5b30f0942bc0591322ba0c9365c78
nakano yosuke <naninuneno0411@gmail.com>
2019-06-21 01:18:26 +0900

    m5stack rotation

--- GrayBlue_Games/Assets/Scripts/BLETestScene.cs
+++ GrayBlue_Games/Assets/Scripts/BLETestScene.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UniRx;
+using GrayBlue;
+using GrayBlue.Rx;
 
 namespace GrayBlue_Games {
     public class BLETestScene : GrayBlueGameSceneBase {
-        protected override void Awake() {
-            base.Awake();
-        }
+        [SerializeField] Transform cube;
+        Peripheral peripheral;
 
-        void Start() {
-            //await SetUpGrayBlueAsync();
+        async void Start() {
+            peripheral = await FindFirstGrayBlueAsync();
+            if (peripheral == null) {
+                return;
+            }
+            var baseQuat = Quaternion.identity;
+            peripheral
+                .ReactiveIMUData()
+                .Take(1)
+                .Subscribe(x => { baseQuat = Quaternion.Inverse(x.unity.quat); })
+                .AddTo(this);
+            peripheral
+                .ReactiveIMUData()
+                .Skip(1)
+                .Subscribe(x => { cube.transform.rotation = baseQuat * x.unity.quat; })
+                .AddTo(this);
+            peripheral.ListenEvent();
         }
     }
 }
