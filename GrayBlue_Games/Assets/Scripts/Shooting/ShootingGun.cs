7954a844a9bd265b165ffba407b6e76251dc9414
nakano yosuke <naninuneno0411@gmail.com>
2019-06-24 23:14:05 +0900

    reticle

---
+++ GrayBlue_Games/Assets/Scripts/Shooting/ShootingGun.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GrayBlue_Games {
+    public class ShootingGun : MonoBehaviour {
+        [SerializeField] Image reticleImage = default;
+        [SerializeField] GameObject[] targets = default;
+
+        Collider[] colliders = default;
+
+        void Awake() {
+            colliders = new Collider[targets.Length];
+            for (var i = 0; i < targets.Length; i++) {
+                colliders[i] = targets[i].transform.GetChild(0).GetComponent<Collider>();
+            }
+        }
+
+        void Update() {
+            // ヒットする方向に向いている時、レティクルの色を変える
+            bool willHit = false;
+            var ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit)) {
+                foreach (var col in colliders) {
+                    if (hit.collider == col) {
+                        willHit = true;
+                        break;
+                    }
+                }
+            }
+            reticleImage.color = willHit ? Color.green : Color.red;
+        }
+
+        public void Shot() {
+
+        }
+    }
+}
