728023fcad66ec751e7e580de7e44d73f21e0c5e
nakano yosuke <naninuneno0411@gmail.com>
2019-06-24 22:08:50 +0900

    gyro reset when A and C pushed

--- GrayBlue_Games/Assets/Scripts/Shooting/GyroShootingScene.cs
+++ GrayBlue_Games/Assets/Scripts/Shooting/GyroShootingScene.cs
@@ -14,12 +14,22 @@ namespace GrayBlue_Games {
         [SerializeField] Toggle gyroReverse = default;
         Peripheral peripheral = default;
         Quaternion cameraBaseQuaternion = Quaternion.identity;
+        Dictionary<ButtonType, bool> btnOnMap = default;
+
+        protected override void Awake() {
+            base.Awake();
+            btnOnMap = new Dictionary<ButtonType, bool> {
+                { ButtonType.Btn_A, false}, { ButtonType.Btn_B, false}, { ButtonType.Btn_C, false}
+            };
+        }
 
         async void Start() {
             peripheral = await FindFirstGrayBlueAsync();
             if (peripheral == null) {
                 return;
             }
+            // toggle変更時にカメラリセット
+            gyroReverse?.onValueChanged.AddListener(_ => { ResetCameraRotation(); });
             var baseQuat = Quaternion.identity;
             // センサ値更新
             peripheral
@@ -37,15 +47,37 @@ namespace GrayBlue_Games {
                     firstPerson.transform.rotation = Quaternion.AngleAxis(pitch, Vector3.right) * Quaternion.AngleAxis(yaw, Vector3.up);
                 })
                 .AddTo(this);
-            // 真ん中ボタン
+            // ボタンの挙動
             peripheral
                 .ButtonPushObservable()
-                .Subscribe(x => {
-                    cameraBaseQuaternion = Quaternion.identity;
-                    firstPerson.transform.rotation = Quaternion.identity;
+                .Subscribe(btn => {
+                    if (btnOnMap.ContainsKey(btn)) {
+                        btnOnMap[btn] = true;
+                    }
+                    // 両端ボタン同時押しでリセット
+                    if (btnOnMap[ButtonType.Btn_A] && btnOnMap[ButtonType.Btn_C]) {
+                        ResetCameraRotation();
+                    }
+                    // 真ん中ボタンでシュート
+                    if (btn == ButtonType.Btn_B) {
+                        Debug.Log("shot");
+                    }
+                })
+                .AddTo(this);
+            peripheral
+                .ButtonReleaseObservable()
+                .Subscribe(btn => {
+                    if (btnOnMap.ContainsKey(btn)) {
+                        btnOnMap[btn] = false;
+                    }
                 })
                 .AddTo(this);
             peripheral.ListenEvent();
+        }
+
+        void ResetCameraRotation() {
+            cameraBaseQuaternion = Quaternion.identity;
+            firstPerson.transform.rotation = Quaternion.identity;
         }
     }
 }
